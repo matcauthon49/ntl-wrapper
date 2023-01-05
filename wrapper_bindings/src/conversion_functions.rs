1a41aa0c858d2f6275b975b40a5b6b9bcbf8866f
Naman Kumar <namankr02@gmail.com>
2023-01-05 11:36:43 +0530

    fixed conversion_functions

--- wrapper_bindings/src/conversion_functions.rs
+++ wrapper_bindings/src/conversion_functions.rs
@@ -1,23 +1,43 @@
-use ark_poly::{self, univariate::DensePolynomial};
 use cty::c_long;
-use wrapper_bindings::ZZ_pX;
+use num::{BigInt, Num};
+use std::ffi::CString;
+
+use ark_poly::{self, univariate::DensePolynomial};
+use ark_bls12_381::Fq as F;
+use ark_std::{UniformRand};
+
 use wrapper_bindings::ZZ_p;
-use wrapper_bindings::ZZ_pX_SetCoeff_fp;
+use wrapper_bindings::ZZ_pX;
+use wrapper_bindings::init_modulus;
+use wrapper_bindings::ZZ_set_string;
+use wrapper_bindings::ZZ_p_set_string;
 use wrapper_bindings::ZZ_pX_zero;
+use wrapper_bindings::ZZ_pX_SetCoeff_fp;
 
 
 pub fn ark_to_NTL_ZZ_p<F: ark_ff::PrimeField>(p: F) -> *mut ZZ_p {
 
-    let p_str: String  =  p.into_repr().to_string();
-    let modulus = F::characteristic();
+    let p_str: *const i8 =  CString::new(BigInt::from_str_radix(
+                                           &p.into_repr()
+                                               .to_string(), 16)
+                                               .unwrap()
+                                               .to_string())
+                                               .unwrap()
+                                               .as_ptr() as *const i8;
 
-    // convert modulus to string, then const char*
-    // ZZ_set_string(modulus);
-    // init_modulus(modulus);
-    // convert p_str to a decimal string, and then to const char*
-    // ZZ_p_set_string(p_str);
-    // return
+    let modulus_u64 = F::characteristic();    
+    let mut modulus_str: String = "".to_owned();
 
+    for i in 0..modulus_u64.len() {
+        modulus_str.push_str(&modulus_u64[i].to_string().to_owned());
+    }
+
+    unsafe {
+        let modulus = ZZ_set_string(CString::new(modulus_str).unwrap().as_ptr() as *const i8);
+        init_modulus(modulus);
+
+        return ZZ_p_set_string(p_str);
+    }
 }
 
 pub fn ark_to_NTL_ZZ_pX<F: ark_ff::PrimeField>(p: DensePolynomial<F>) -> *mut ZZ_pX {
@@ -38,5 +58,14 @@ pub fn ark_to_NTL_ZZ_pX<F: ark_ff::PrimeField>(p: DensePolynomial<F>) -> *mut ZZ
 
 
 pub fn main() {
+
+    
+    let mut rng = ark_std::rand::thread_rng();
+    
+    let a = F::rand(&mut rng);
+    let b = F::rand(&mut rng);
+
+    println!("{}", a);
+    ark_to_NTL_ZZ_p(a);
 
 }
